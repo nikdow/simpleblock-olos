4b8e23a3db9c6996676306a69f63584ca6c3e4d7
cbdweb <nik@cbdweb.net>
2017-05-07 22:42:53 +1000

    bug: parent style handle in enqueue of child theme;

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function olos_enqueue_styles() {
     
-    $parent_style = 'parent-style';
+    $parent_style = 'main';
     
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', 
